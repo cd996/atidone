37c37fed547b8001b2cc43725aa54a41040bdd43
cd <cdhj996@gmail.com>
2024-10-10 17:51:35 +0800

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2024-07-30',
+  future: { compatibilityVersion: 4 },
+  modules: [
+    '@nuxthub/core',
+    '@nuxt/ui',
+    'nuxt-auth-utils',
+    '@nuxt/eslint'
+  ],
+  hub: {
+    database: true
+  },
+  // Development config
+  eslint: {
+    config: {
+      stylistic: {
+        quotes: 'single',
+        commaDangle: 'never'
+      }
+    }
+  },
+
+  vite: {
+    server: {
+      hmr: {
+        protocol: 'wss'
+      }
+    }
+  },
+  devtools: {
+    enabled: true
+  }
+})
